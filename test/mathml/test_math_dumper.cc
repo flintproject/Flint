edc9ee3fc98936810bf44543ed0ded2202eb0dbb
Takeshi Abe <takeshi.abe@oist.jp>
2015-04-03 13:24:59 +0900

    add unit test for mathml/math_dumper.h

---
+++ test/mathml/test_math_dumper.cc
@@ -0,0 +1,92 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
+#include "mathml/math_dumper.h"
+
+#include <sstream>
+#define BOOST_TEST_MODULE test_math_dumper
+#include <boost/test/unit_test.hpp>
+
+#define TEST_MATHML_0(dirname, basename) (#dirname "/" basename)
+#define TEST_MATHML_1(dirname, basename) TEST_MATHML_0(dirname, basename)
+#define TEST_MATHML(basename) TEST_MATHML_1(TEST_XML_MODELS, basename)
+
+struct GF {
+	GF() {
+		LIBXML_TEST_VERSION
+		xmlInitParser();
+	}
+	~GF() {
+		xmlCleanupParser();
+	}
+};
+
+BOOST_GLOBAL_FIXTURE(GF)
+
+class Handler {
+public:
+	int Handle(int i) const {return i;}
+};
+
+struct F {
+	F() : text_reader_(NULL),
+		  os_()
+	{}
+
+	~F() {
+		if (text_reader_) xmlFreeTextReader(text_reader_);
+		os_.str("");
+	}
+
+	void ReadAndDump(const char *filename,
+							const char *expected) {
+		text_reader_ = xmlReaderForFile(filename, NULL, 0);
+		BOOST_CHECK(text_reader_ != NULL);
+		Handler handler;
+		mathml::MathDumper dumper(text_reader_, &os_);
+		int i = dumper.Read(&handler);
+		BOOST_CHECK_EQUAL(i, 0);
+		BOOST_CHECK_EQUAL(os_.str().c_str(), expected);
+	}
+
+	xmlTextReaderPtr text_reader_;
+	std::ostringstream os_;
+};
+
+BOOST_FIXTURE_TEST_SUITE(test_math_dumper, F)
+
+BOOST_AUTO_TEST_CASE(mathml_cosh)
+{
+	ReadAndDump(TEST_MATHML("mathml_cosh.xml"),
+				" math (eq (cosh %x) (divide (plus (exp %x) (exp (minus %x))) 2))");
+}
+
+BOOST_AUTO_TEST_CASE(mathml_matrix)
+{
+	ReadAndDump(TEST_MATHML("mathml_matrix.xml"),
+				" math (eq %M (matrix (matrixrow 0 1 0) (matrixrow 0 0 1) (matrixrow 1 0 0)))");
+}
+
+BOOST_AUTO_TEST_CASE(mathml_piecewise)
+{
+	ReadAndDump(TEST_MATHML("mathml_piecewise.xml"),
+				" math (eq (abs %x) (piecewise (piece (minus %x) (lt %x 0)) (piece 0 (eq %x 0)) (piece %x (gt %x 0))))");
+}
+
+BOOST_AUTO_TEST_CASE(mathml_sinh)
+{
+	ReadAndDump(TEST_MATHML("mathml_sinh.xml"),
+				" math (eq (sinh %x) (divide (minus (exp %x) (exp (minus %x))) 2))");
+}
+
+BOOST_AUTO_TEST_CASE(mathml_tanh)
+{
+	ReadAndDump(TEST_MATHML("mathml_tanh.xml"),
+				" math (eq (tanh %x) (divide (sinh %x) (cosh %x)))");
+}
+
+BOOST_AUTO_TEST_CASE(mathml_vector)
+{
+	ReadAndDump(TEST_MATHML("mathml_vector.xml"),
+				" math (eq %V (vector 1 2.34e-10 3/4 %x))");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
