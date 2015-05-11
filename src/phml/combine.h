672832c195cbef14b641c85bc1ba087c0ee79fde
Takeshi Abe <takeshi.abe@oist.jp>
2015-05-11 14:41:30 +0900

    refactor flint-combine's code

---
+++ src/phml/combine.h
@@ -0,0 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
+#ifndef FLINT_PHML_COMBINE_H_
+#define FLINT_PHML_COMBINE_H_
+
+bool Combine(const char *uuid,
+			 const char *db_file,
+			 const char *name_file,
+			 const char *value_file,
+			 const char *function_file,
+			 const char *ode_file);
+
+#endif
