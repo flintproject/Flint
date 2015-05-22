eca7ac6a949f7002e1996792efa5e1629a2ea690
Takeshi Abe <takeshi.abe@oist.jp>
2015-05-22 15:42:21 +0900

    fixtures

--- test/sbml/test_load.cc
+++ test/sbml/test_load.cc
@@ -12,21 +12,29 @@
 #include "test.hh"
 
 struct F {
+
+	~F() {
+		if (db_file) std::remove(db_file);
+	}
+
+	const char *db_file;
 };
 
 BOOST_FIXTURE_TEST_SUITE(test_load, F)
 
 BOOST_AUTO_TEST_CASE(BIOMD0000000114) {
-	BOOST_CHECK_EQUAL(SaveGivenFile("BIOMD0000000114.db", TEST_MODELS("BIOMD0000000114.xml")), 1);
-	BOOST_CHECK(flint::sbml::Parse("BIOMD0000000114.db"));
-	db::Driver driver("BIOMD0000000114.db");
+	db_file = "BIOMD0000000114.db";
+	BOOST_CHECK_EQUAL(SaveGivenFile(db_file, TEST_MODELS("BIOMD0000000114.xml")), 1);
+	BOOST_CHECK(flint::sbml::Parse(db_file));
+	db::Driver driver(db_file);
 	BOOST_CHECK(flint::sbml::Load(driver.db()));
 }
 
 BOOST_AUTO_TEST_CASE(BIOMD0000000152) {
-	BOOST_CHECK_EQUAL(SaveGivenFile("BIOMD0000000152.db", TEST_MODELS("BIOMD0000000152.xml")), 1);
-	BOOST_CHECK(flint::sbml::Parse("BIOMD0000000152.db"));
-	db::Driver driver("BIOMD0000000152.db");
+	db_file = "BIOMD0000000152.db";
+	BOOST_CHECK_EQUAL(SaveGivenFile(db_file, TEST_MODELS("BIOMD0000000152.xml")), 1);
+	BOOST_CHECK(flint::sbml::Parse(db_file));
+	db::Driver driver(db_file);
 	BOOST_CHECK(flint::sbml::Load(driver.db()));
 }
 
