9a20f9a0f6c0773aa5e1a597dd871fc48dbe0453
Takeshi Abe <takeshi.abe@oist.jp>
2015-06-10 16:58:13 +0900

    extract phz::Read() from flint-phz's code

--- src/flint-phz-main.cc
+++ src/flint-phz-main.cc
@@ -4,31 +4,16 @@
 #endif
 
 #include <cassert>
-#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <string>
-#include <iostream>
 
-#define BOOST_FILESYSTEM_NO_DEPRECATED
-#include <boost/filesystem.hpp>
-#include <boost/scoped_array.hpp>
-#include <boost/scoped_ptr.hpp>
-
-#include <zip.h>
-
-#include "database.h"
 #include "db/driver.h"
-#include "modelpath.h"
-#include "utf8path.h"
+#include "phz.hh"
 
 using std::cerr;
 using std::endl;
-using std::perror;
 using std::strcmp;
-using std::string;
-using std::strncmp;
 
 namespace {
 
@@ -54,113 +39,5 @@ int main(int argc, char *argv[])
 	}
 
 	db::Driver driver(argv[1]);
-
-	boost::scoped_array<char> filename(GetGivenFilename(driver.db()));
-	int ze;
-	struct zip *zp = zip_open(filename.get(), 0, &ze);
-	if (!zp) {
-		char buf[1024];
-		int len = zip_error_to_str(buf, 1023, ze, errno);
-		assert(len < 1024);
-		buf[len] = '\0';
-		cerr << buf << endl;
-		return EXIT_FAILURE;
-	}
-	zip_int64_t ne = zip_get_num_entries(zp, 0);
-	if (ne < 0) {
-		cerr << "the zip archive is null: "
-			 << filename.get()
-			 << endl;
-		return EXIT_FAILURE;
-	}
-	if (ne == 0) {
-		cerr << "the zip archive is empty: "
-			 << filename.get()
-			 << endl;
-		return EXIT_FAILURE;
-	}
-
-	boost::filesystem::path tp(argv[2]);
-
-	for (zip_int64_t i=0;i<ne;i++) {
-		const char *name = zip_get_name(zp, i, 0);
-		if (!name) {
-			cerr << zip_strerror(zp) << endl;
-			return EXIT_FAILURE;
-		}
-		size_t nlen = strlen(name);
-		if (nlen <= 1) {
-			cerr << "unexpected entry in the zip archive: "
-				 << name
-				 << endl;
-			continue;
-		} else if (name[nlen-1] == '/') {
-			// skip a directory entry
-			continue;
-		}
-
-		boost::filesystem::path op(tp);
-		op /= name;
-		boost::filesystem::path pp(op.parent_path());
-		boost::filesystem::create_directories(pp);
-
-		string op_s = op.string();
-		FILE *ofp = fopen(op_s.c_str(), "wb");
-		if (!ofp) {
-			perror(op_s.c_str());
-			return EXIT_FAILURE;
-		}
-
-		struct zip_file *zfp = zip_fopen_index(zp, i, 0);
-		if (!zfp) {
-			cerr << zip_strerror(zp) << endl;
-			return EXIT_FAILURE;
-		}
-
-		char buf[1024];
-		int len;
-		while ( (len = zip_fread(zfp, buf, 1024)) ) {
-			if (len < 0) {
-				cerr << "failed to read entry of index "
-					 << i
-					 << " in the zip archive: "
-					 << filename.get()
-					 << endl;
-				return EXIT_FAILURE;
-			}
-			assert(len <= 1024);
-			if (fwrite(buf, len, 1, ofp) != 1) {
-				cerr << "failed to write entry of index "
-					 << i
-					 << " in the zip archive: "
-					 << filename.get()
-					 << endl;
-				return EXIT_FAILURE;
-			}
-		}
-		zip_fclose(zfp);
-
-		fclose(ofp);
-	}
-	zip_close(zp);
-
-	tp /= "model.phml";
-	if (!boost::filesystem::is_regular_file(tp)) {
-		cerr << "missing model.phml in the zip archive: "
-			 << filename.get()
-			 << endl;
-		return EXIT_FAILURE;
-	}
-
-	boost::filesystem::path amp = boost::filesystem::absolute(tp);
-	boost::scoped_array<char> mf(GetUtf8FromPath(amp));
-	if (strlen(mf.get()) >= 1024) {
-		cerr << "resulting filename is too long: "
-			 << mf.get()
-			 << endl;
-		return EXIT_FAILURE;
-	}
-	if (!SaveModelFile(driver.db(), mf.get())) return EXIT_FAILURE;
-
-	return EXIT_SUCCESS;
+	return phz::Read(driver.db(), argv[2]) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
---
+++ src/phz/read.cc
@@ -0,0 +1,145 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "phz.hh"
+
+#include <cassert>
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+#include <boost/filesystem.hpp>
+#include <boost/scoped_array.hpp>
+
+#include <zip.h>
+
+#include "database.h"
+#include "modelpath.h"
+#include "utf8path.h"
+
+using std::cerr;
+using std::endl;
+using std::perror;
+using std::string;
+
+namespace phz {
+
+bool Read(sqlite3 *db, const char *dir)
+{
+	boost::scoped_array<char> filename(GetGivenFilename(db));
+	int ze;
+	struct zip *zp = zip_open(filename.get(), 0, &ze);
+	if (!zp) {
+		char buf[1024];
+		int len = zip_error_to_str(buf, 1023, ze, errno);
+		assert(len < 1024);
+		buf[len] = '\0';
+		cerr << buf << endl;
+		return false;
+	}
+	zip_int64_t ne = zip_get_num_entries(zp, 0);
+	if (ne < 0) {
+		cerr << "the zip archive is null: "
+			 << filename.get()
+			 << endl;
+		return false;
+	}
+	if (ne == 0) {
+		cerr << "the zip archive is empty: "
+			 << filename.get()
+			 << endl;
+		return false;
+	}
+
+	boost::filesystem::path tp(dir);
+
+	for (zip_int64_t i=0;i<ne;i++) {
+		const char *name = zip_get_name(zp, i, 0);
+		if (!name) {
+			cerr << zip_strerror(zp) << endl;
+			return false;
+		}
+		size_t nlen = strlen(name);
+		if (nlen <= 1) {
+			cerr << "unexpected entry in the zip archive: "
+				 << name
+				 << endl;
+			continue;
+		} else if (name[nlen-1] == '/') {
+			// skip a directory entry
+			continue;
+		}
+
+		boost::filesystem::path op(tp);
+		op /= name;
+		boost::filesystem::path pp(op.parent_path());
+		boost::filesystem::create_directories(pp);
+
+		string op_s = op.string();
+		FILE *ofp = fopen(op_s.c_str(), "wb");
+		if (!ofp) {
+			perror(op_s.c_str());
+			return false;
+		}
+
+		struct zip_file *zfp = zip_fopen_index(zp, i, 0);
+		if (!zfp) {
+			cerr << zip_strerror(zp) << endl;
+			return false;
+		}
+
+		char buf[1024];
+		int len;
+		while ( (len = zip_fread(zfp, buf, 1024)) ) {
+			if (len < 0) {
+				cerr << "failed to read entry of index "
+					 << i
+					 << " in the zip archive: "
+					 << filename.get()
+					 << endl;
+				return false;
+			}
+			assert(len <= 1024);
+			if (fwrite(buf, len, 1, ofp) != 1) {
+				cerr << "failed to write entry of index "
+					 << i
+					 << " in the zip archive: "
+					 << filename.get()
+					 << endl;
+				return false;
+			}
+		}
+		zip_fclose(zfp);
+
+		fclose(ofp);
+	}
+	zip_close(zp);
+
+	tp /= "model.phml";
+	if (!boost::filesystem::is_regular_file(tp)) {
+		cerr << "missing model.phml in the zip archive: "
+			 << filename.get()
+			 << endl;
+		return false;
+	}
+
+	boost::filesystem::path amp = boost::filesystem::absolute(tp);
+	boost::scoped_array<char> mf(GetUtf8FromPath(amp));
+	if (strlen(mf.get()) >= 1024) {
+		cerr << "resulting filename is too long: "
+			 << mf.get()
+			 << endl;
+		return false;
+	}
+	if (!SaveModelFile(db, mf.get())) return false;
+
+	return true;
+}
+
+}
