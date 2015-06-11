f72dd6e1ea83619a14d16d804c412da3a8217325
Takeshi Abe <takeshi.abe@oist.jp>
2015-06-11 10:11:14 +0900

    add a missing header

---
+++ src/phz.hh
@@ -0,0 +1,17 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
+#ifndef FLINT_PHZ_HH_
+#define FLINT_PHZ_HH_
+
+#include "sqlite3.h"
+
+namespace phz {
+
+/*
+ * Read a PHZ file.
+ * Return true in case of success, false otherwise.
+ */
+bool Read(sqlite3 *db, const char *dir);
+
+}
+
+#endif
