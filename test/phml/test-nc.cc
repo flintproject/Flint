3d75ebf4f9449fb40ce254f7bb990d2fa46b6bdf
Takeshi Abe <takeshi.abe@oist.jp>
2015-06-24 11:50:01 +0900

    add unit tests for phml::Nc()

---
+++ test/phml/test-nc.cc
@@ -0,0 +1,70 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "phml.hh"
+
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include "database.h"
+#include "db/driver.h"
+
+#define BOOST_TEST_MODULE test_nc
+#include "test.hh"
+
+struct F {
+
+	F()
+		: driver_("") // generating a temporary file
+	{}
+
+	void Phml(const char *file) {
+		SaveGivenFile(driver_.db(), file);
+		BOOST_REQUIRE(phml::Read(driver_.db()));
+	}
+
+	void GenerateAndCheck(const char *output, const char *expected)
+	{
+		BOOST_REQUIRE(phml::Nc(driver_.db(), output));
+		BOOST_REQUIRE(boost::filesystem::is_regular_file(output));
+		boost::filesystem::remove(output);
+
+		test::Sql sql(driver_.db());
+		std::vector<std::string> config;
+		sql.Table("config", &config);
+		BOOST_CHECK_EQUAL(config.size(), 1u);
+		BOOST_CHECK_EQUAL(config[0], expected);
+	}
+
+	db::Driver driver_;
+};
+
+BOOST_FIXTURE_TEST_SUITE(test_nc, F)
+
+BOOST_AUTO_TEST_CASE(bogus_td) {
+	Phml(TEST_MODELS("bogus-td.phml"));
+	GenerateAndCheck("bogus-td.phml.nc",
+					 "rk4 ");
+}
+
+BOOST_AUTO_TEST_CASE(Izhikevich_2003_model) {
+	Phml(TEST_MODELS("Izhikevich_2003_model.isml"));
+	GenerateAndCheck("Izhikevich_2003_model.isml.nc",
+					 "rk4 ");
+}
+
+BOOST_AUTO_TEST_CASE(LR1_ring_reentry_initiate) {
+	Phml(TEST_MODELS("LR1-ring_reentry_initiate.isml"));
+	GenerateAndCheck("LR1-ring_reentry_initiate.isml.nc",
+					 "4th-rungekutta ");
+}
+
+BOOST_AUTO_TEST_CASE(Rybak_2006_with_static_instance_and_multiple_input) {
+	Phml(TEST_MODELS("Rybak_2006_with_static_instance_and_multiple_input.isml"));
+	GenerateAndCheck("Rybak_2006_with_static_instance_and_multiple_input.isml.nc",
+					 "euler ");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
