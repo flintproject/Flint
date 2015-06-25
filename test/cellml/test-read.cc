ad39cf38169cf08b6c239a7568263d4316ed3238
Takeshi Abe <takeshi.abe@oist.jp>
2015-06-25 11:59:39 +0900

    add unit tests for cellml::Read()

---
+++ test/cellml/test-read.cc
@@ -0,0 +1,35 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "cellml.hh"
+
+#include "database.h"
+#include "db/driver.hh"
+
+#define BOOST_TEST_MODULE test_read
+#include "test.hh"
+
+struct F {
+
+	F()
+		: driver_("") // generating a temporary file
+	{}
+
+	db::Driver driver_;
+};
+
+BOOST_FIXTURE_TEST_SUITE(test_read, F)
+
+BOOST_AUTO_TEST_CASE(lockwood_ewy_hermann_holford_2006) {
+	BOOST_REQUIRE_EQUAL(SaveGivenFile(driver_.db(), TEST_MODELS("lockwood_ewy_hermann_holford_2006.cellml")), 1);
+	BOOST_CHECK(cellml::Read(driver_.db()));
+}
+
+BOOST_AUTO_TEST_CASE(yang_tong_mccarver_hines_beard_2006) {
+	BOOST_REQUIRE_EQUAL(SaveGivenFile(driver_.db(), TEST_MODELS("yang_tong_mccarver_hines_beard_2006.cellml")), 1);
+	BOOST_CHECK(cellml::Read(driver_.db()));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
