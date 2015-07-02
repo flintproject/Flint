3b4adab1df91626a6c8a9705344e3b78b1d2a535
Takeshi Abe <takeshi.abe@oist.jp>
2015-07-02 13:46:00 +0900

    for-each

--- flint/src/jp/oist/flint/plotter/PlotterLoader.java
+++ flint/src/jp/oist/flint/plotter/PlotterLoader.java
@@ -26,8 +26,7 @@ public class PlotterLoader {
         Preferences prefs = Preferences.userRoot().node(PLOTTER_DIRECTORY);
         String[] keys = prefs.childrenNames();
         int c = 0;
-        for (int i=0; i < keys.length; i++) {
-            String key = keys[i];
+        for (String key : keys) {
             try {
                 IPlotter plotter = load(key);
                 comboBox.addItem(plotter);
