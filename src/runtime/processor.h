flintproject/Flint

commit 2b2f4b94136722c09ec60536f9f1d20563c9c8c2

prefer std::ordered_set to std::set
since the client code can take advantage of amortized constant time
for find()/insert().

src/runtime/processor.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <iterator>
 #include <memory>
+#include <unordered_set>
 #include <set>
 #include <string>
 
@@ -285,9 +286,9 @@ public:
 	int block_index() const {return block_index_;}
 	const std::set<int> &flow_addrs() const {return flow_addrs_;}
 
-	void Insert(int offset, const std::set<int> &flow_addrs) {
-		for (std::set<int>::const_iterator it=flow_addrs.begin();it!=flow_addrs.end();++it) {
-			flow_addrs_.insert(offset + *it);
+	void Insert(int offset, const std::unordered_set<int> &flow_addrs) {
+		for (int a : flow_addrs) {
+			flow_addrs_.insert(offset + a);
 		}
 	}
 
@@ -463,8 +464,7 @@ public:
 
 			FlowOutboundMap::iterator sobit = sub_outbound.find(source);
 			if (sobit == sub_outbound.end()) continue;
-			for (std::set<int>::const_iterator tit=sobit->second->begin();tit!=sobit->second->end();++tit) {
-				int target = *tit;
+			for (int target : *sobit->second) {
 				output[target] += output[source];
 				disposition[target] = disposition[source] + 1;
 				// remove an (inbound) edge
@@ -854,8 +854,8 @@ private:
 			for (int i=0;i<nol;i++) {
 				if (i%2 == 0) { // only on an even layer
 					FlowDependency *fd = new FlowDependency(it->first + (i * layer_size_));
-					for (std::set<int>::const_iterator tit=it->second->begin();tit!=it->second->end();++tit) {
-						fd->AddSourceAddr(*tit + (i * layer_size_));
+					for (int src : *it->second) {
+						fd->AddSourceAddr(src + (i * layer_size_));
 					}
 					fdv->push_back(fd);
 				}
@@ -879,11 +879,11 @@ private:
 		}
 	}
 
-	void AnnotateFlow(const FlowOutboundMap *outbound, const std::set<int> &addrs,
+	void AnnotateFlow(const FlowOutboundMap *outbound, const std::unordered_set<int> &addrs,
 					  int offset, ExecutionUnit *eu) {
 		eu->Insert(offset, addrs);
-		for (std::set<int>::const_iterator it=addrs.begin();it!=addrs.end();++it) {
-			FlowOutboundMap::const_iterator fit = outbound->find(*it);
+		for (int a : addrs) {
+			FlowOutboundMap::const_iterator fit = outbound->find(a);
 			if (fit == outbound->end()) continue;
 			AnnotateFlow(outbound, *fit->second, offset, eu);
 		}

src/job/evolve.cc
@@ -159,8 +159,7 @@ public:
 	void Communicate(const FlowInboundMap *inbound) {
 		for (FlowInboundMap::const_iterator it=inbound->begin();it!=inbound->end();++it) {
 			int dst = it->first;
-			for (std::set<int>::const_iterator tit=it->second->begin();tit!=it->second->end();++tit) {
-				int src = *tit;
+			for (int src : *it->second) {
 				if (color_[src]) {
 					data_[dst] += data_[src];
 				} else {

src/runtime/flow.hh
@@ -2,13 +2,13 @@
 #ifndef FLINT_RUNTIME_FLOW_HH_
 #define FLINT_RUNTIME_FLOW_HH_
 
-#include <set>
-#include <boost/ptr_container/ptr_map.hpp>
+#include <unordered_set>
+#include <boost/ptr_container/ptr_unordered_map.hpp>
 
 #include "sqlite3.h"
 
-typedef boost::ptr_map<int, std::set<int> > FlowInboundMap;
-typedef boost::ptr_map<int, std::set<int> > FlowOutboundMap;
+typedef boost::ptr_unordered_map<int, std::unordered_set<int> > FlowInboundMap;
+typedef boost::ptr_unordered_map<int, std::unordered_set<int> > FlowOutboundMap;
 
 /*
  * Note that db is for read only.

commit 1c3c810a65c9e0eb339e423a69d863553493e3f5

input for std::set_difference() must be sorted
This fixes a regression from 2b2f4b94136722c09ec60536f9f1d20563c9c8c2.

src/runtime/processor.h
@@ -434,12 +434,13 @@ public:
 			for (std::set<int>::iterator it=offsets.begin();it!=offsets.end();++it) {
 				FlowOutboundMap::iterator obit = outbound->find(*it);
 				if (obit == outbound->end()) continue;
+				// make targets ordered for std::set_difference()
+				std::set<int> targets(obit->second->begin(), obit->second->end());
+				outbound->erase(obit);
 				std::set<int> diff;
-				std::set_difference(obit->second->begin(), obit->second->end(),
+				std::set_difference(targets.begin(), targets.end(),
 									constants.begin(), constants.end(),
 									std::inserter(diff, diff.begin()));
-				outbound->erase(obit);
-				if (diff.empty()) continue;
 				for (std::set<int>::const_iterator dit=diff.begin();dit!=diff.end();++dit) {
 					sub_inbound[*dit].insert(*it);
 					sub_outbound[*it].insert(*dit);