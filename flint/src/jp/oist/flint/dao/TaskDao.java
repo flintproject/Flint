97bc8336a95a522bc51eac25020301a21998c053
Takeshi Abe <takeshi.abe@oist.jp>
2015-07-31 11:14:32 +0900

    do not depend on the default locale

--- flint/src/jp/oist/flint/dao/TaskDao.java
+++ flint/src/jp/oist/flint/dao/TaskDao.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class TaskDao extends DaoObject {
@@ -24,7 +25,7 @@ public class TaskDao extends DaoObject {
         public static Status fromString (String s) {
             if (s == null || s.isEmpty())
                 return UNKNOWN;
-            switch (s.toLowerCase()) {
+            switch (s.toLowerCase(Locale.ENGLISH)) {
             case "pending":
                 return PENDING;
             case "generated":
