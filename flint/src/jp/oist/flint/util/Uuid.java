469deefe48562844f6a292a20a5e79bd02e9b0e5
Takeshi Abe <takeshi.abe@oist.jp>
2015-08-07 13:45:34 +0900

    add a class to format raw bytes representing a UUID

---
+++ flint/src/jp/oist/flint/util/Uuid.java
@@ -0,0 +1,17 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 et: */
+package jp.oist.flint.util;
+
+public class Uuid {
+
+    public static String fromByteArray(byte[] bytes) {
+        assert bytes.length == 16;
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<16;i++) {
+            sb.append(String.format("%02x", bytes[i]));
+            if (i == 3 | i == 5 | i == 7 | i == 9)
+                sb.append('-');
+        }
+        return sb.toString();
+    }
+
+}
