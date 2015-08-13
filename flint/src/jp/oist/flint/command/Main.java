e50c225b46623300378b0fbcaf401bfceafd8b09
Takeshi Abe <takeshi.abe@oist.jp>
2015-08-13 17:24:57 +0900

    simplify the main class

--- flint/src/jp/oist/flint/command/Main.java
+++ flint/src/jp/oist/flint/command/Main.java
@@ -4,7 +4,6 @@ package jp.oist.flint.command;
 import org.apache.log4j.Logger;
 import java.lang.reflect.InvocationTargetException;
 import javax.swing.SwingUtilities;
-import jp.oist.flint.form.MainFrame;
 
 public class Main {
 
@@ -13,38 +12,15 @@ public class Main {
      */
     public static void main(final String args[]) {
         try {
-
-            new Main(args);
-
+            CommandLineArguments commandLineArgs = new CommandLineArguments();
+            commandLineArgs.parse(args);
+            Setup setup = new Setup(commandLineArgs);
+            SwingUtilities.invokeAndWait(setup);
+            Runnable launcher = setup.getLauncher();
+            SwingUtilities.invokeAndWait(launcher);
         } catch (InvocationTargetException | InterruptedException ex) {
             Logger.getRootLogger().fatal(ex.getMessage());
             System.exit(1);
         }
-    }
-
-    private final MainFrame mMainFrame;
-
-    public Main (String[] args) throws InvocationTargetException, InterruptedException {
-        CommandLineArguments commandLineArgs = new CommandLineArguments();
-        commandLineArgs.parse(args);
-
-        Setup setup = new Setup(commandLineArgs);
-
-        SwingUtilities.invokeAndWait(setup);
-
-        Runnable launcher = setup.getLauncher();
-
-        SwingUtilities.invokeAndWait(launcher);
-
-        if (launcher instanceof Launch) {
-            mMainFrame = ((Launch)launcher).getMainFrame();
-        } else {
-            mMainFrame = null;
-        }
-    }
-        
-
-    public MainFrame getMainFrame () {
-        return mMainFrame;
     }
 }
