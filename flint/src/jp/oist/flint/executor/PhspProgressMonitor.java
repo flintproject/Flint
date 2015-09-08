e71ffeca027ea7f8d58a06e4fdef636b38a3bf55
Takeshi Abe <takeshi.abe@oist.jp>
2015-09-08 13:57:59 +0900

    Stop monitoring the directory
    This also abandon use of Commons VFS.

--- flint/src/jp/oist/flint/executor/PhspProgressMonitor.java
+++ flint/src/jp/oist/flint/executor/PhspProgressMonitor.java
@@ -8,77 +8,40 @@ import jp.oist.flint.job.Job;
 import jp.oist.flint.job.Progress;
 import jp.oist.flint.sedml.ISimulationConfiguration;
 import jp.oist.flint.sedml.ISimulationConfigurationList;
-import org.apache.log4j.Logger;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import javax.swing.SwingUtilities;
-import org.apache.commons.vfs2.FileChangeEvent;
-import org.apache.commons.vfs2.FileListener;
-import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
-import org.apache.commons.vfs2.FileSystemManager;
-import org.apache.commons.vfs2.VFS;
-import org.apache.commons.vfs2.impl.DefaultFileMonitor;
 
-
-public class PhspProgressMonitor implements FileListener, Runnable {
+public class PhspProgressMonitor implements Runnable {
 
     private final SimulationDao mSimulationDao;
 
     private final ISimulationConfigurationList mSimulationConfigurationList;
 
-    private final DefaultFileMonitor mFileMonitor;
-
     private final PropertyChangeSupport mPropetyChangeSupport;
 
     private final Thread mObserver;
 
-    private final Object mLock = new Object();
-
     private volatile boolean mDone = false;
 
-    private final LinkedList<Job> mQueue = new LinkedList<>();
-
-    public PhspProgressMonitor(PhspSimulator simulator)
-            throws FileSystemException {
+    public PhspProgressMonitor(PhspSimulator simulator) {
         mSimulationDao = simulator.getSimulationDao();
         mSimulationConfigurationList = simulator.getSimulationConfigurationList();
         mObserver = new Thread(this);
         mPropetyChangeSupport = new PropertyChangeSupport(this);
-
-        FileSystemManager mgr = VFS.getManager();
-        mFileMonitor = new DefaultFileMonitor(this) {
-            @Override
-            protected void queueAddFile (FileObject fileObj) {
-                if (isTaskDirectory(fileObj) ||
-                    isJobDirectory(fileObj))
-                    super.queueAddFile(fileObj);
-            }
-        };
-        mFileMonitor.setDelay(200);
-        mFileMonitor.addFile(mgr.toFileObject(simulator.getWorkingDirectory()));
     }
 
     public void start () {
-        mFileMonitor.start();
         mObserver.start();
     }
 
     public void stop () {
-        mFileMonitor.stop();
         mDone = true;
-        try {
-            mObserver.join();
-        } catch (InterruptedException ex) {
-            Logger.getRootLogger().error(ex.getMessage());
-            //ignored.
-        }
     }
 
     public PropertyChangeSupport getPropertyChangeSupport () {
@@ -134,111 +97,30 @@ public class PhspProgressMonitor implements FileListener, Runnable {
     }
 
     @Override
-    public final void fileCreated(FileChangeEvent evt) throws IOException {
-        FileObject fileObj = evt.getFile();
-        if (isJobDirectory(fileObj)) {
-            String baseName = fileObj.getParent().getName().getBaseName();
-            int taskId = Integer.parseInt(baseName);
-
-            baseName = fileObj.getName().getBaseName();
-            int jobId = Integer.parseInt(baseName);
+    public void run() {
+        while (!mDone) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+            }
 
             try {
-                TaskDao task = mSimulationDao.obtainTask(taskId);
-                Job job = task.obtainJob(jobId);
-
-                if (task.isCancelled())
-                    job.cancel();
-
-                setProgress(taskId, jobId, job.getProgress());
-
-                synchronized (mLock) {
-                    mQueue.addFirst(job);
+                int taskCount = mSimulationDao.getCount();
+                for (int i=1; i<=taskCount; i++) {
+                    TaskDao task = mSimulationDao.obtainTask(i);
+                    if (!task.isStarted())
+                        continue;
+                    int jobCount = task.getCount();
+                    for (int j=1; j<=jobCount; j++) {
+                        Job job = task.obtainJob(j);
+                        setProgress(i, j, job.getProgress());
+                    }
                 }
             } catch (DaoException | IOException | SQLException ex) {
                 // give up
             }
         }
-    }
 
-    @Override
-    public final void fileDeleted(FileChangeEvent evt) {
-        // nothing to do...
-    }
-
-    @Override 
-    public final void fileChanged(FileChangeEvent evt) {
-        // nothing to do...
-    }
-
-    @Override
-    public void run() {
-        while (!mDone) {
-            synchronized (mLock) {
-                for (int i=0; i<mQueue.size(); i++) {
-                    Job job = mQueue.get(i);
-
-                    Progress progress = job.getProgress();
-                    int taskId = job.getTaskId();
-                    int jobId  = job.getJobId();
-                    setProgress(taskId, jobId, progress);
-
-                    if (progress.isCompleted() || job.isCancelled())
-                        mQueue.remove(i);
-                }
-            }
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-            }
-        }
-
-        try {
-            int taskCount = mSimulationDao.getCount();
-            for (int i=1; i<=taskCount; i++) {
-                TaskDao task = mSimulationDao.obtainTask(i);
-                int jobCount = task.getCount();
-                for (int j=1; j<=jobCount; j++) {
-                    Job job = task.obtainJob(j);
-                    setProgress(i, j, job.getProgress());
-                }
-            }
-        } catch (DaoException | IOException | SQLException ex) {
-            // give up
-        }
-    }
-
-    public boolean isTaskDirectory(FileObject fileObj) {
-        try {
-            return isNaturalNumber(fileObj.getName().getBaseName())
-                    && !isNaturalNumber(fileObj.getParent().getName().getBaseName());
-        } catch (FileSystemException ex) {
-            return false;
-        }
-    }
-
-    public boolean isJobDirectory(FileObject fileObj) {
-        try {
-            return isNaturalNumber(fileObj.getName().getBaseName())
-                    && isNaturalNumber(fileObj.getParent().getName().getBaseName());
-        } catch (FileSystemException ex) {
-            return false;
-        }
-    }
-
-    private boolean isNaturalNumber (String s) {
-        try {
-            long l = Long.parseLong(s);
-
-            return isNaturalNumber(l);
-        } catch (NumberFormatException ex) {
-            return false;
-        }
-    }
-
-    private boolean isNaturalNumber (Long l) {
-        return l > 0;
     }
 
     public static class Event extends PropertyChangeEvent {
