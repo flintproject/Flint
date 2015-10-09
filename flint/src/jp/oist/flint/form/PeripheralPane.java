2905a9ae6d37acf8e42ae94842a870f5fc8d8f5d
Takeshi Abe <takeshi.abe@oist.jp>
2015-10-09 14:43:14 +0900

    Remove nonsense

--- flint/src/jp/oist/flint/form/PeripheralPane.java
+++ flint/src/jp/oist/flint/form/PeripheralPane.java
@@ -1,34 +1,17 @@
 /* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 et: */
-
 package jp.oist.flint.form;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
 import javax.swing.JPanel;
 
-public abstract class PeripheralPane extends JPanel {
-
-    private Container mContentPane;
+abstract class PeripheralPane extends JPanel {
 
     public PeripheralPane() {
         super(new BorderLayout());
-
-        initComponents();
     }
 
-    private void initComponents () {
-        mContentPane = new JPanel();
-
-        add(mContentPane, BorderLayout.CENTER);
-    }
-
-    public void setContentPane (Container contentPane) {
-        remove(mContentPane);
-        mContentPane = contentPane;
-        add(mContentPane);
-    }
-
-    public Container getContentPane () {
-        return mContentPane;
+    public void setContentPane(Container contentPane) {
+        add(contentPane);
     }
 }
