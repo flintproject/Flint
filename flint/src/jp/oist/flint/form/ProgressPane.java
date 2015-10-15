135f901b086bb84c65d2c7c075b907989883f3a4
Takeshi Abe <takeshi.abe@oist.jp>
2015-10-15 10:30:58 +0900

    Merge ProgressList into ProgressPane for simplicity

--- flint/src/jp/oist/flint/form/ProgressPane.java
+++ flint/src/jp/oist/flint/form/ProgressPane.java
@@ -8,34 +8,47 @@ import jp.oist.flint.executor.PhspSimulator;
 import jp.oist.flint.form.sub.SubFrame;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import javax.swing.DefaultListModel;
+import java.util.ArrayList;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class ProgressPane extends PeripheralPane
     implements IDesktopListener, ISimulationListener {
 
-    private final ProgressList mProgressList;
+    private final ArrayList<ProgressCell> mCells;
+
+    private final JPanel mPanel;
+
+    private final JScrollPane mScrollPane;
 
     public ProgressPane() {
-        mProgressList = new ProgressList();
-        setContentPane(mProgressList.createPane());
+        mCells = new ArrayList<>();
+
+        mPanel = new JPanel();
+        mPanel.setLayout(new BoxLayout(mPanel, BoxLayout.PAGE_AXIS));
+
+        mScrollPane = new JScrollPane(mPanel);
+        setContentPane(mScrollPane);
     }
 
-    public void setSelectedCell (ProgressCell plcp, boolean selected) {
-        mProgressList.setSelectedCell(plcp, selected);
-    }
-
-    private List<ProgressCell> getListCells() {
-        return mProgressList.getCells();
-    }
-
-    private void removeListCell(ProgressCell cell) {
-        mProgressList.remove(cell);
+    public void setSelectedCell(ProgressCell cell, boolean selected) {
+        if (!selected) {
+            cell.setSelected(false);
+            return;
+        }
+        for (ProgressCell other : mCells) {
+            if (cell == other) {
+                other.setSelected(true);
+            } else {
+                other.setSelected(false);
+            }
+        }
     }
 
     public ProgressCell getListCellOfModel (File modelFile) {
-        for (ProgressCell cell : getListCells()) {
+        for (ProgressCell cell : mCells) {
             if (cell.getDocument().getFile().equals(modelFile))
                 return cell;
         }
@@ -43,7 +56,7 @@ public class ProgressPane extends PeripheralPane
     }
 
     public void prepareJobWindows(PhspSimulator simulator) throws IOException, ParserConfigurationException {
-        for (ProgressCell cell : getListCells())
+        for (ProgressCell cell : mCells)
             cell.prepareJobWindow(simulator);
     }
 
@@ -52,18 +65,22 @@ public class ProgressPane extends PeripheralPane
     @Override
     public void documentAdded(Document doc) {
         final SubFrame subFrame = doc.getSubFrame();
-        ProgressCell plcp = new ProgressCell(doc);
-        plcp.addMouseListener(new ProgressCellMouseListener(subFrame));
-        mProgressList.add(plcp);
-        mProgressList.setSelectedCell(plcp, true);
-        subFrame.setStatusComponent(plcp);
+        ProgressCell cell = new ProgressCell(doc);
+        cell.addMouseListener(new ProgressCellMouseListener(subFrame));
+        mCells.add(cell);
+        mPanel.add(cell);
+        mPanel.repaint();
+        setSelectedCell(cell, true);
+        subFrame.setStatusComponent(cell);
     }
 
     @Override
     public void documentRemoved(Document doc, boolean empty) {
-        for (ProgressCell cell : getListCells()) {
+        for (ProgressCell cell : mCells) {
             if (doc.equals(cell.getDocument())) {
-                removeListCell(cell);
+                mCells.remove(cell);
+                mPanel.remove(cell);
+                mPanel.repaint();
                 break;
             }
         }
@@ -73,7 +90,7 @@ public class ProgressPane extends PeripheralPane
 
     @Override
     public void simulationStarted(PhspSimulator simulator) {
-        for (ProgressCell cell : getListCells())
+        for (ProgressCell cell : mCells)
             cell.progressStarted();
         repaint();
     }
