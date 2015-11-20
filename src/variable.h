76215d6d66e76e93fd470c8a3aa9fc711eb90d5e
Takeshi Abe <takeshi.abe@oist.jp>
2015-11-20 15:31:19 +0900

    Fix #include guard

--- src/variable.h
+++ src/variable.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
-#ifndef FLINT_NAME_H_
-#define FLINT_NAME_H_
+#ifndef FLINT_VARIABLE_H_
+#define FLINT_VARIABLE_H_
 
 #include <string>
 
