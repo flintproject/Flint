f4f1929d4e1305ddde122bcff7076b4b4312e826
Takeshi Abe <takeshi.abe@oist.jp>
2015-12-04 17:48:35 +0900

    Fix #include guard

--- src/runtime.hh
+++ src/runtime.hh
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
-#ifndef FLINT_RUNTIME_INIT_HH_
-#define FLINT_RUNTIME_INIT_HH_
+#ifndef FLINT_RUNTIME_HH_
+#define FLINT_RUNTIME_HH_
 
 #include "sqlite3.h"
 
