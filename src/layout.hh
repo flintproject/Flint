3e07b8d8b6cd3f0caca6025ae1b07942d669585e
Takeshi Abe <takeshi.abe@oist.jp>
2015-12-04 17:45:32 +0900

    Fix #include guard

--- src/layout.hh
+++ src/layout.hh
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
-#ifndef FLINT_LAYOUT_GENERATE_HH_
-#define FLINT_LAYOUT_GENERATE_HH_
+#ifndef FLINT_LAYOUT_HH_
+#define FLINT_LAYOUT_HH_
 
 #include "sqlite3.h"
 
