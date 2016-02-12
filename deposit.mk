77ffcffb334b91502a1d9d0376e4b9cce930a98f
Takeshi Abe <takeshi.abe@oist.jp>
2016-02-12 14:29:26 +0900

    Upgrade libzip to 1.1.1

--- deposit.mk
+++ deposit.mk
@@ -6,7 +6,7 @@ SOSLIB_COMMIT = 87bfbc8d
 BOOST_VERSION = 1.60.0
 LIBSBML_VERSION = 5.11.4
 LIBXML2_VERSION = 2.7.8
-LIBZIP_VERSION = 1.0.1
+LIBZIP_VERSION = 1.1.1
 PROTOBUF_VERSION = 2.6.1
 SUNDIALS_VERSION = 2.6.2
 ZLIB_VERSION = 1.2.8
@@ -70,7 +70,7 @@ $(eval $(call external_library_source,$(BOOST_UNDERSCORE).tar.bz2,65a840e1a0b13a
 $(eval $(call external_library_source,clibsedml-$(CLIBSEDML_COMMIT).tar.gz,d780b4f02d5272e3c0b2d4610e6d6c3f))
 $(eval $(call external_library_source,libSBML-$(LIBSBML_VERSION)-core-src.zip,d854f58ba65c685692bff6031b78db27))
 $(eval $(call external_library_source,libxml2-sources-$(LIBXML2_VERSION).tar.gz,a78857dd73a8784776d7f9625ccf7a39))
-$(eval $(call external_library_source,libzip-$(LIBZIP_VERSION).tar.gz,a8c3d356221b9e69ff54c1d53cc39e92))
+$(eval $(call external_library_source,libzip-$(LIBZIP_VERSION).tar.gz,133aefc4c7e45a1b7d168a617e289ef6))
 $(eval $(call external_library_source,protobuf-$(PROTOBUF_VERSION).tar.gz,f3916ce13b7fcb3072a1fa8cf02b2423))
 $(eval $(call external_library_source,SBML_odeSolver-$(SOSLIB_COMMIT).tar.gz,c44cf16d5c7aab2176faeafa8cacb62d))
 $(eval $(call external_library_source,sundials-$(SUNDIALS_VERSION).tar.gz,3deeb0ede9f514184c6bd83ecab77d95))
