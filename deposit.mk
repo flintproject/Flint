55baf0713556e80bc36545608ed3b32067c66dab
Takeshi Abe <takeshi.abe@oist.jp>
2016-06-16 14:25:26 +0900

    Download tarballs from established sites

--- deposit.mk
+++ deposit.mk
@@ -43,35 +43,35 @@ src:
 ifeq ($(OS),Darwin)
 
 define external_library_source
-src/$(1): | src
-	curl $(DEPOSIT_URL)/$(1) -o $$@
-	md5 $$@ | grep $(2)
+src/$(2): | src
+	curl -o $$@ $(1)/$(2)
+	md5 $$@ | grep $(3)
 endef
 
 else ifeq ($(OS),FreeBSD)
 
 define external_library_source
-src/$(1): | src
-	curl $(DEPOSIT_URL)/$(1) -o $$@
-	md5 $$@ | grep $(2)
+src/$(2): | src
+	curl -o $$@ $(1)/$(2)
+	md5 $$@ | grep $(3)
 endef
 
 else
 
 define external_library_source
-src/$(1): | src
-	wget -O $$@ $(DEPOSIT_URL)/$(1)
-	test "$(2)" = `md5sum $$@ | cut -d' ' -f1`
+src/$(2): | src
+	wget -O $$@ $(1)/$(2)
+	test "$(3)" = `md5sum $$@ | cut -d' ' -f1`
 endef
 
 endif
 
-$(eval $(call external_library_source,$(BOOST_UNDERSCORE).tar.bz2,6095876341956f65f9d35939ccea1a9f))
-$(eval $(call external_library_source,clibsedml-$(CLIBSEDML_COMMIT).tar.gz,d780b4f02d5272e3c0b2d4610e6d6c3f))
-$(eval $(call external_library_source,libSBML-$(LIBSBML_VERSION)-core-src.zip,d854f58ba65c685692bff6031b78db27))
-$(eval $(call external_library_source,libxml2-sources-$(LIBXML2_VERSION).tar.gz,a78857dd73a8784776d7f9625ccf7a39))
-$(eval $(call external_library_source,libzip-$(LIBZIP_VERSION).tar.gz,133aefc4c7e45a1b7d168a617e289ef6))
-$(eval $(call external_library_source,protobuf-$(PROTOBUF_VERSION).tar.gz,f3916ce13b7fcb3072a1fa8cf02b2423))
-$(eval $(call external_library_source,SBML_odeSolver-$(SOSLIB_COMMIT).tar.gz,c44cf16d5c7aab2176faeafa8cacb62d))
-$(eval $(call external_library_source,sundials-$(SUNDIALS_VERSION).tar.gz,3deeb0ede9f514184c6bd83ecab77d95))
-$(eval $(call external_library_source,zlib-$(ZLIB_VERSION).tar.gz,44d667c142d7cda120332623eab69f40))
+$(eval $(call external_library_source,http://downloads.sourceforge.net/project/boost/boost/$(BOOST_VERSION),$(BOOST_UNDERSCORE).tar.bz2,6095876341956f65f9d35939ccea1a9f))
+$(eval $(call external_library_source,$(DEPOSIT_URL),clibsedml-$(CLIBSEDML_COMMIT).tar.gz,d780b4f02d5272e3c0b2d4610e6d6c3f))
+$(eval $(call external_library_source,http://downloads.sourceforge.net/project/sbml/libsbml/$(LIBSBML_VERSION)/stable,libSBML-$(LIBSBML_VERSION)-core-src.zip,d854f58ba65c685692bff6031b78db27))
+$(eval $(call external_library_source,http://xmlsoft.org/sources,libxml2-sources-$(LIBXML2_VERSION).tar.gz,a78857dd73a8784776d7f9625ccf7a39))
+$(eval $(call external_library_source,http://www.nih.at/libzip,libzip-$(LIBZIP_VERSION).tar.gz,133aefc4c7e45a1b7d168a617e289ef6))
+$(eval $(call external_library_source,https://github.com/google/protobuf/releases/download/v$(PROTOBUF_VERSION),protobuf-$(PROTOBUF_VERSION).tar.gz,f3916ce13b7fcb3072a1fa8cf02b2423))
+$(eval $(call external_library_source,$(DEPOSIT_URL),SBML_odeSolver-$(SOSLIB_COMMIT).tar.gz,c44cf16d5c7aab2176faeafa8cacb62d))
+$(eval $(call external_library_source,http://pkgs.fedoraproject.org/repo/extras/sundials/sundials-$(SUNDIALS_VERSION).tar.gz/3deeb0ede9f514184c6bd83ecab77d95,sundials-$(SUNDIALS_VERSION).tar.gz,3deeb0ede9f514184c6bd83ecab77d95))
+$(eval $(call external_library_source,http://zlib.net,zlib-$(ZLIB_VERSION).tar.gz,44d667c142d7cda120332623eab69f40))
