21c7a6e1de030f7d9b019cb39a4566945d651c1d
Takeshi Abe <takeshi.abe@oist.jp>
2017-02-22 21:05:45 +0900

    config.h comes first

--- src/phml/combine-all.cc
+++ src/phml/combine-all.cc
@@ -1,9 +1,9 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
-#include "phml.h"
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+
+#include "phml.h"
 
 #include <cassert>
 #include <cstdio>
