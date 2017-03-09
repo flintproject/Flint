flintproject/Flint

commit 17dcc632d5eb813edd9e6d04b936506fdfd88119

Upgrade version-info for Protocol Buffers 3.2

mingw-w64/Makefile
@@ -197,7 +197,7 @@ $(foreach j,$(JARS),$(eval $(call flint_jar,$(j))))
 
 opt/bin/libODES-0.dll: tmp/SBML_odeSolver-$(SOSLIB_VERSION).installed
 
-opt/bin/libprotobuf-11.dll: tmp/protobuf-$(PROTOBUF_VERSION).installed
+opt/bin/libprotobuf-$(PROTOBUF_CURRENT).dll: tmp/protobuf-$(PROTOBUF_VERSION).installed
 
 opt/bin/libsedml-0.dll: tmp/clibsedml-$(CLIBSEDML_COMMIT).installed
 
@@ -245,7 +245,7 @@ flint/flint.xml: flint.xml | flint
 	install -p -m 0644 $< $@
 
 flint/flint.wxs: flint.wxs.in | flint
-	m4 -DVERSION=$(VERSION) -DLIBGCC_DLL=$(LIBGCC_DLL) -DLIBSTDCXX_DLL=$(LIBSTDCXX_DLL) -DLIBWINPTHREAD_DLL=$(LIBWINPTHREAD_DLL) $< > $@
+	m4 -DVERSION=$(VERSION) -DLIBGCC_DLL=$(LIBGCC_DLL) -DLIBSTDCXX_DLL=$(LIBSTDCXX_DLL) -DLIBWINPTHREAD_DLL=$(LIBWINPTHREAD_DLL) -DPROTOBUF_CURRENT=$(PROTOBUF_CURRENT)$< > $@
 
 flint/icon.png: ../flint/src/jp/oist/flint/image/icon.png | flint
 	install -p -m 0644 $< $@

rpm.el6/Makefile
@@ -72,7 +72,7 @@ endef
 
 define flint_spec
 flint-$(1).spec: flint-$(1).spec.in
-	m4 -D$(1)_VERSION=$($(1)_VERSION) -D$(1)_RELEASE=$($(1)_RELEASE) -DGTK_PACKAGE_NAME=$(GTK_PACKAGE_NAME) $$< > $$@
+	m4 -D$(1)_VERSION=$($(1)_VERSION) -D$(1)_RELEASE=$($(1)_RELEASE) -DGTK_PACKAGE_NAME=$(GTK_PACKAGE_NAME) -DPROTOBUF_CURRENT=$(PROTOBUF_CURRENT) $$< > $$@
 endef
 
 define flint_rpmbuild

xcode/Makefile
@@ -29,7 +29,7 @@ DYLIBS := \
     libsbml.$(LIBSBML_VERSION) \
     libsedml.0 \
     libxml2.2 \
-    libprotobuf.11 \
+    libprotobuf.$(PROTOBUF_CURRENT) \
     libwx_osx_cocoau-3.1.0.0.0 \
     libzip.4
 SUNDIALS_DYLIBS := \

deposit.mk
@@ -12,6 +12,8 @@ SUNDIALS_VERSION = 2.7.0
 WXWIDGETS_VERSION = 3.1.0
 ZLIB_VERSION = 1.2.11
 
+PROTOBUF_CURRENT = 12
+
 BOOST_UNDERSCORE = boost_$(subst .,_,$(BOOST_VERSION))
 
 EXTERNAL_LIBRARIES = \

mingw-w64/flint.wxs.in
@@ -159,7 +159,7 @@
             <File Id='libsedml' Name='libsedml-0.dll' DiskId='1' Source='libsedml-0.dll' />
           </Component>
           <Component Id='protobuf' Guid='233d044c-ff5f-11e4-bcee-2f0a32a0ce0d'>
-            <File Id='libprotobuf' Name='libprotobuf-11.dll' DiskId='1' Source='libprotobuf-11.dll' />
+            <File Id='libprotobuf' Name='libprotobuf-PROTOBUF_CURRENT.dll' DiskId='1' Source='libprotobuf-PROTOBUF_CURRENT.dll' />
           </Component>
           <Component Id='sbml' Guid='e23853ca-ff5e-11e4-a61d-873a99f64190'>
             <File Id='libsbml' Name='libsbml.dll' DiskId='1' Source='libsbml.dll' />

mingw-w64/header.mk
@@ -12,7 +12,7 @@ JARS := \
     sqlite-jdbc-3.14.2.1
 BIN_DLLS := \
     libODES-0 \
-    libprotobuf-11 \
+    libprotobuf-$(PROTOBUF_CURRENT) \
     libsbml \
     libsedml-0 \
     libxml2-2 \

rpm.el6/flint-protobuf.spec.in
@@ -43,18 +43,18 @@ rm -rf %{buildroot}
 /opt/flint/lib/libprotobuf-lite.a
 /opt/flint/lib/libprotobuf-lite.la
 /opt/flint/lib/libprotobuf-lite.so
-/opt/flint/lib/libprotobuf-lite.so.12
-/opt/flint/lib/libprotobuf-lite.so.12.0.0
+/opt/flint/lib/libprotobuf-lite.so.PROTOBUF_CURRENT
+/opt/flint/lib/libprotobuf-lite.so.PROTOBUF_CURRENT.0.0
 /opt/flint/lib/libprotobuf.a
 /opt/flint/lib/libprotobuf.la
 /opt/flint/lib/libprotobuf.so
-/opt/flint/lib/libprotobuf.so.12
-/opt/flint/lib/libprotobuf.so.12.0.0
+/opt/flint/lib/libprotobuf.so.PROTOBUF_CURRENT
+/opt/flint/lib/libprotobuf.so.PROTOBUF_CURRENT.0.0
 /opt/flint/lib/libprotoc.a
 /opt/flint/lib/libprotoc.la
 /opt/flint/lib/libprotoc.so
-/opt/flint/lib/libprotoc.so.12
-/opt/flint/lib/libprotoc.so.12.0.0
+/opt/flint/lib/libprotoc.so.PROTOBUF_CURRENT
+/opt/flint/lib/libprotoc.so.PROTOBUF_CURRENT.0.0
 /opt/flint/lib/pkgconfig/protobuf-lite.pc
 /opt/flint/lib/pkgconfig/protobuf.pc
 

commit c3d02cc53b579b9786ff34f1af069f1b7a5eb4b1

Stop including header.mk
It turns out that MSI_DEPENDENCY is unnecessary because flint.wxs
can tell the whole dependencies to create .msi. This fixes another
regression from 17dcc632d5eb813edd9e6d04b936506fdfd88119.

mingw-w64/Makefile
@@ -240,9 +240,6 @@ flint/$(LIBWINPTHREAD_DLL): $(LIBWINPTHREAD_DIR)/$(LIBWINPTHREAD_DLL) | flint
 flint/Makefile: flint.mk | flint
 	install -p -m 0644 $< $@
 
-flint/header.mk: header.mk | flint
-	install -p -m 0644 $< $@
-
 flint/flint.ico: flint.ico | flint
 	install -p -m 0644 $< $@
 
@@ -295,7 +292,7 @@ flint/third-party-licenses.txt: tmp/libsbml-$(LIBSBML_VERSION) tmp/libxml2-$(LIB
 	echo >> $@
 	cat var/zeromq-$(ZEROMQ_VERSION)/COPYING.LESSER >> $@
 
-flint.zip: $(foreach e,$(EXES),$(call flint_exe_name,$(e))) $(foreach d,$(BOOST_DLLS) $(SUNDIALS_DLLS) wxmsw310u_gcc_custom $(FLINT_DLLS),$(call flint_dll_name,$(d))) flint/$(LIBGCC_DLL) flint/$(LIBSTDCXX_DLL) flint/$(LIBWINPTHREAD_DLL) flint/flint.jar $(foreach j,$(JARS),$(call flint_jar_name,$(j))) flint/Makefile flint/header.mk flint/flint.ico flint/flint.xml flint/flint.wxs flint/icon.png flint/license.rtf flint/third-party-licenses.txt
+flint.zip: $(foreach e,$(EXES),$(call flint_exe_name,$(e))) $(foreach d,$(BOOST_DLLS) $(SUNDIALS_DLLS) wxmsw310u_gcc_custom $(FLINT_DLLS),$(call flint_dll_name,$(d))) flint/$(LIBGCC_DLL) flint/$(LIBSTDCXX_DLL) flint/$(LIBWINPTHREAD_DLL) flint/flint.jar $(foreach j,$(JARS),$(call flint_jar_name,$(j))) flint/Makefile flint/flint.ico flint/flint.xml flint/flint.wxs flint/icon.png flint/license.rtf flint/third-party-licenses.txt
 	zip -r flint flint
 
 .DEFAULT_GOAL := all

mingw-w64/flint.mk
@@ -1,12 +1,4 @@
 # -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
-include header.mk
-
-MSI_DEPENDENCY = \
-    flint.jar \
-    $(foreach j,$(JARS),lib/$(j).jar) \
-    $(foreach d,$(BIN_DLLS) $(BOOST_DLLS) wxmsw310u_gcc_custom,$(d).dll) \
-    $(foreach e,$(EXES),$(e).exe)
-
 .PHONY: all clean install uninstall timestamp
 
 all: timestamp
@@ -26,7 +18,7 @@ flint.exe: flint.xml
 flint.wixobj: flint.wxs
 	candle $<
 
-flint.msi: flint.wixobj flint.exe $(MSI_DEPENDENCY)
+flint.msi: flint.wixobj flint.exe
 	light -ext WixUIExtension $<
 
 timestamp: flint.msi