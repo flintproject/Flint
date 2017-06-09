c70e635afc1079faba8e685c7b5ec41ea6e2235f
Takeshi Abe <takeshi.abe@oist.jp>
2017-06-09 10:41:59 +0900

    Bump up version for next major release 1.9

--- flint/src/jp/oist/flint/form/AboutDialog.java
+++ flint/src/jp/oist/flint/form/AboutDialog.java
@@ -13,7 +13,7 @@ import java.util.ResourceBundle;
 public class AboutDialog extends javax.swing.JDialog {
 
     private final static String URL = "https://flintproject.github.io/";
-    private final static String VERSION = "1.8";
+    private final static String VERSION = "1.9";
 
     public AboutDialog(java.awt.Frame parent) {
         super(parent, true);
--- src/gui/main-frame.cc
+++ src/gui/main-frame.cc
@@ -296,7 +296,7 @@ void MainFrame::OnAbout(wxCommandEvent &)
 {
 	wxAboutDialogInfo aboutInfo;
 	aboutInfo.SetName("Flint");
-	aboutInfo.SetVersion("1.8");
+	aboutInfo.SetVersion("1.9");
 	aboutInfo.SetDescription("A simulator for biological and physiological models");
 	aboutInfo.SetCopyright("(C) 2015-2017 Okinawa Institute of Science and Technology Graduate University");
 	aboutInfo.SetWebSite("https://flintproject.github.io/");
