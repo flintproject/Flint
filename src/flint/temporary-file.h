7b0f03215ff8c9561658cb205d7f3f88db8b4c7a
Takeshi Abe <takeshi.abe@oist.jp>
2017-06-27 13:17:29 +0900

    Fix typo

--- src/flint/temporary-file.h
+++ src/flint/temporary-file.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
-#ifndef FLINT_TEMORARY_FILE_H_
-#define FLINT_TEMORARY_FILE_H_
+#ifndef FLINT_TEMPORARY_FILE_H_
+#define FLINT_TEMPORARY_FILE_H_
 
 #define BOOST_FILESYSTEM_NO_DEPRECATED
 #include <boost/filesystem.hpp>
