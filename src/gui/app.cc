29a7050856d8e16794b3ba1122c8beb993389e92
Takeshi Abe <takeshi.abe@oist.jp>
2017-09-08 15:25:13 +0900

    Support the headless mode

--- src/gui/app.cc
+++ src/gui/app.cc
@@ -14,9 +14,16 @@
 #include <wx/persist.h>
 #pragma GCC diagnostic pop
 
+#include "cli.pb.h"
 #include "gui/main-frame.h"
 #include "gui/pref-page-general.h"
+#include "run.h"
 #include "utf8path.h"
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <limits>
 
 namespace flint {
 namespace gui {
@@ -66,9 +73,41 @@ wxString PersistentApp::GetName() const
 const wxCmdLineEntryDesc kCommandLineDesc[] =
 {
     { wxCMD_LINE_SWITCH, "h", "help", "show this help message", wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
-    { wxCMD_LINE_PARAM, nullptr, nullptr, "input file", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL|wxCMD_LINE_PARAM_MULTIPLE },
+    { wxCMD_LINE_SWITCH, "headless", "headless", "enable the headless mode", wxCMD_LINE_VAL_NONE, 0 },
+    { wxCMD_LINE_OPTION, "e", nullptr,
+	  "save error messages as specified file (only with the headless mode)",
+	  wxCMD_LINE_VAL_STRING, 0 },
+    { wxCMD_LINE_OPTION, "g", nullptr,
+	  "specify output sampling rate i.e. 1 output per given step (only with the headless mode)",
+	  wxCMD_LINE_VAL_NUMBER, 0 },
+    { wxCMD_LINE_OPTION, "s", nullptr,
+	  "choose output variables with specified file (only with the headless mode)",
+	  wxCMD_LINE_VAL_STRING, 0 },
+    { wxCMD_LINE_PARAM, nullptr, nullptr, "file", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL|wxCMD_LINE_PARAM_MULTIPLE },
     { wxCMD_LINE_NONE, nullptr, nullptr, nullptr, wxCMD_LINE_VAL_NONE, 0 } // the same as { wxCMD_LINE_NONE }, and suppress compiler warnig
 };
+
+/*
+ * We need this as we have to change directory before simulation.
+ */
+std::string GetAbsoluteFilename(const wxString &file)
+{
+	wxFileName fileName(file);
+	if (!fileName.IsAbsolute())
+		fileName.MakeAbsolute();
+	return fileName.GetFullPath().ToStdString();
+}
+
+void ChangeDirectory()
+{
+	wxFileName fileName;
+	fileName.AssignHomeDir();
+	fileName.AppendDir(".flint");
+	fileName.AppendDir("2");
+	fileName.Rmdir(wxPATH_RMDIR_RECURSIVE); // clean up working directory at first
+	fileName.Mkdir(wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL); // make sure that it exists
+	fileName.SetCwd();
+}
 
 }
 
@@ -89,13 +128,7 @@ bool App::OnInit()
 
 	wxPersistenceManager::Get().RegisterAndRestore(this, new PersistentApp(this, gnuplot_executable_));
 
-	wxFileName fileName;
-	fileName.AssignHomeDir();
-	fileName.AppendDir(".flint");
-	fileName.AppendDir("2");
-	fileName.Rmdir(wxPATH_RMDIR_RECURSIVE); // clean up working directory at first
-	fileName.Mkdir(wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL); // make sure that it exists
-	fileName.SetCwd();
+	ChangeDirectory();
 
 	auto frame = new MainFrame(input_files_);
 	SetTopWindow(frame);
@@ -114,9 +147,46 @@ void App::OnInitCmdLine(wxCmdLineParser &parser)
 
 bool App::OnCmdLineParsed(wxCmdLineParser &parser)
 {
+	if (parser.Found("headless")) {
+		// the headless mode
+		if (parser.GetParamCount() != 2) {
+			std::cerr << "just 2 arguments (i.e. <input> and <output>) needed for the headless mode" << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
+		wxString param0 = parser.GetParam(0);
+		wxString param1 = parser.GetParam(1);
+		cli::RunOption option;
+		option.set_model_filename(GetAbsoluteFilename(param0));
+		option.set_output_filename(GetAbsoluteFilename(param1));
+		wxString e;
+		if (parser.Found("e", &e))
+			option.set_error_filename(GetAbsoluteFilename(e));
+		long g;
+		if (parser.Found("g", &g)) {
+			if (g <= 0) {
+				std::cerr << "non-positive value is invalid for -g option: "
+						  << g
+						  << std::endl;
+				std::exit(EXIT_FAILURE);
+			}
+			if (std::numeric_limits<int>::max() < g) {
+				std::cerr << "too huge value for -g option: "
+						  << g
+						  << std::endl;
+				std::exit(EXIT_FAILURE);
+			}
+			option.set_granularity(static_cast<int>(g));
+		}
+		wxString s;
+		if (parser.Found("s", &s))
+			option.set_spec_filename(GetAbsoluteFilename(s));
+		ChangeDirectory();
+		std::exit(run::Run(option) ? EXIT_SUCCESS : EXIT_FAILURE);
+	}
+
 	for (const auto &arg : parser.GetArguments()) {
 		if (arg.GetKind() == wxCMD_LINE_PARAM)
-			input_files_.Add(arg.GetStrVal());
+			input_files_.Add(GetAbsoluteFilename(arg.GetStrVal()));
 	}
 	return true;
 }
