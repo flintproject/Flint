a32b52cc04393d614b56a7f08943aecaccc55e9d
Takeshi Abe <tabe@fixedpoint.jp>
2018-03-23 10:52:30 +0900

    Upgrade ZeroMQ to 4.2.4

--- deposit.mk
+++ deposit.mk
@@ -9,7 +9,7 @@ PROTOBUF_VERSION = 3.5.0
 SOSLIB_VERSION = 1.9.0
 SUNDIALS_VERSION = 2.7.0
 WXWIDGETS_VERSION = 3.1.1
-ZEROMQ_VERSION = 4.2.3
+ZEROMQ_VERSION = 4.2.4
 
 CERES_CURRENT = 1
 CZMQ_CURRENT = 4
@@ -18,7 +18,7 @@ PROTOBUF_CURRENT = 15
 ZEROMQ_CURRENT = 5
 
 CZMQ_VERSION_INFO = $(CZMQ_CURRENT).1.0
-ZEROMQ_VERSION_INFO = $(ZEROMQ_CURRENT).1.3
+ZEROMQ_VERSION_INFO = $(ZEROMQ_CURRENT).1.4
 
 BOOST_UNDERSCORE = boost_$(subst .,_,$(BOOST_VERSION))
 
@@ -110,4 +110,4 @@ $(eval $(call external_library_source,https://github.com/google/protobuf/release
 $(eval $(call external_library_source3,https://github.com/raim/SBML_odeSolver/archive/$(SOSLIB_VERSION).tar.gz,SBML_odeSolver-$(SOSLIB_VERSION).tar.gz,059c2f51f52e1ac29d3873a2d3fcc71947e1a196f29816067041c976cb5cc651))
 $(eval $(call external_library_source,https://github.com/LLNL/sundials/releases/download/v$(SUNDIALS_VERSION),sundials-$(SUNDIALS_VERSION).tar.gz,d39fcac7175d701398e4eb209f7e92a5b30a78358d4a0c0fcc23db23c11ba104))
 $(eval $(call external_library_source,https://github.com/wxWidgets/wxWidgets/releases/download/v$(WXWIDGETS_VERSION),wxWidgets-$(WXWIDGETS_VERSION).tar.bz2,c925dfe17e8f8b09eb7ea9bfdcfcc13696a3e14e92750effd839f5e10726159e))
-$(eval $(call external_library_source,https://github.com/zeromq/libzmq/releases/download/v$(ZEROMQ_VERSION),zeromq-$(ZEROMQ_VERSION).tar.gz,8f1e2b2aade4dbfde98d82366d61baef2f62e812530160d2e6d0a5bb24e40bc0))
+$(eval $(call external_library_source,https://github.com/zeromq/libzmq/releases/download/v$(ZEROMQ_VERSION),zeromq-$(ZEROMQ_VERSION).tar.gz,1ad5c28d44e31f0f7cad09b95bafaca8cc5346ff24a63a5e7d01c954162ad536))
