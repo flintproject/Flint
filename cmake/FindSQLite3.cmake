b7308558b584dafbe599c2443110881a187201b0
Takeshi Abe <tabe@fixedpoint.jp>
2019-07-12 11:16:56 +0900

    Find FindPackageHandleStandardArgs.cmake in CMake's root directory

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -48,6 +48,7 @@ if(SQLite3_INCLUDE_DIR)
     unset(_ver_line)
 endif()
 
+include("${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake")
 find_package_handle_standard_args(SQLite3
     REQUIRED_VARS SQLite3_INCLUDE_DIR SQLite3_LIBRARY
     VERSION_VAR SQLite3_VERSION)
