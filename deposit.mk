0bc8a3b9262b24f1aeaac709b3e34c93c2f1c59a
Takeshi Abe <tabe@fixedpoint.jp>
2019-11-27 09:14:07 +0900

    Upgrade (bundled) libxml2 to 2.9.10

--- deposit.mk
+++ deposit.mk
@@ -5,7 +5,7 @@ CLIBSEDML_VERSION = 0.1.2
 CZMQ_VERSION = 4.2.0
 LIBMICROHTTPD_VERSION = 0.9.64
 LIBSBML_VERSION = 5.17.0
-LIBXML2_VERSION = 2.9.7
+LIBXML2_VERSION = 2.9.10
 PROTOBUF_VERSION = 3.10.0
 SOSLIB_VERSION = 1.9.0
 SUNDIALS_VERSION = 2.7.0
@@ -106,7 +106,7 @@ $(eval $(call external_library_source3,https://github.com/flintproject/clibsedml
 $(eval $(call external_library_source,https://github.com/zeromq/czmq/releases/download/v$(CZMQ_VERSION),czmq-$(CZMQ_VERSION).tar.gz,cfab29c2b3cc8a845749758a51e1dd5f5160c1ef57e2a41ea96e4c2dcc8feceb))
 $(eval $(call external_library_source,https://ftp.gnu.org/gnu/libmicrohttpd,libmicrohttpd-$(LIBMICROHTTPD_VERSION).tar.gz,e792d8ed5990823a0baadea0adf94365999e702f6f1314ef9c555018dafc350e))
 $(eval $(call external_library_source,http://downloads.sourceforge.net/project/sbml/libsbml/$(LIBSBML_VERSION)/stable,libSBML-$(LIBSBML_VERSION)-core-src.zip,76d6c1e9bbe966204db602d9595b6536fc96ff6af7404d2ca1df3225a0a721b4))
-$(eval $(call external_library_source,ftp://xmlsoft.org/libxml2,libxml2-$(LIBXML2_VERSION).tar.gz,f63c5e7d30362ed28b38bfa1ac6313f9a80230720b7fb6c80575eeab3ff5900c))
+$(eval $(call external_library_source,ftp://xmlsoft.org/libxml2,libxml2-$(LIBXML2_VERSION).tar.gz,aafee193ffb8fe0c82d4afef6ef91972cbaf5feea100edc2f262750611b4be1f))
 $(eval $(call external_library_source,https://github.com/google/protobuf/releases/download/v$(PROTOBUF_VERSION),protobuf-cpp-$(PROTOBUF_VERSION).tar.gz,ffb91e102e8c389fba6fefec948421852979ae655ebb52e69859a6a3f4c5b61b))
 $(eval $(call external_library_source3,https://github.com/raim/SBML_odeSolver/archive/$(SOSLIB_VERSION).tar.gz,SBML_odeSolver-$(SOSLIB_VERSION).tar.gz,059c2f51f52e1ac29d3873a2d3fcc71947e1a196f29816067041c976cb5cc651))
 $(eval $(call external_library_source,https://github.com/LLNL/sundials/releases/download/v$(SUNDIALS_VERSION),sundials-$(SUNDIALS_VERSION).tar.gz,d39fcac7175d701398e4eb209f7e92a5b30a78358d4a0c0fcc23db23c11ba104))
