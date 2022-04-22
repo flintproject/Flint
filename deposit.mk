74bfd6ed36e38c3b3fe68e864bfed58445f04308
Takeshi Abe <tabe@fixedpoint.jp>
2022-04-22 17:03:56 +0900

    Upgrade (bundled) GNU Libmicrohttpd to 0.9.75

--- deposit.mk
+++ deposit.mk
@@ -3,7 +3,7 @@ BOOST_VERSION = 1.77.0
 CERES_VERSION = 1.14.0
 CLIBSEDML_VERSION = 0.1.2
 CZMQ_VERSION = 4.2.1
-LIBMICROHTTPD_VERSION = 0.9.73
+LIBMICROHTTPD_VERSION = 0.9.75
 LIBSBML_VERSION = 5.17.0
 LIBXML2_VERSION = 2.9.13
 PROTOBUF_VERSION = 3.18.0
@@ -107,7 +107,7 @@ $(eval $(call external_library_source,https://boostorg.jfrog.io/artifactory/main
 $(eval $(call external_library_source,http://ceres-solver.org/,ceres-solver-$(CERES_VERSION).tar.gz,4744005fc3b902fed886ea418df70690caa8e2ff6b5a90f3dd88a3d291ef8e8e))
 $(eval $(call external_library_source3,https://github.com/flintproject/clibsedml/archive/v$(CLIBSEDML_VERSION).tar.gz,clibsedml-$(CLIBSEDML_VERSION).tar.gz,cf25d0aad379e14c26a74166b5d652e2072fc87183caf6f34c16b5122ffa469f))
 $(eval $(call external_library_source,https://github.com/zeromq/czmq/releases/download/v$(CZMQ_VERSION),czmq-$(CZMQ_VERSION).tar.gz,5d720a204c2a58645d6f7643af15d563a712dad98c9d32c1ed913377daa6ac39))
-$(eval $(call external_library_source,https://ftp.gnu.org/gnu/libmicrohttpd,libmicrohttpd-$(LIBMICROHTTPD_VERSION).tar.gz,a37b2f1b88fd1bfe74109586be463a434d34e773530fc2a74364cfcf734c032e))
+$(eval $(call external_library_source,https://ftp.gnu.org/gnu/libmicrohttpd,libmicrohttpd-$(LIBMICROHTTPD_VERSION).tar.gz,9278907a6f571b391aab9644fd646a5108ed97311ec66f6359cebbedb0a4e3bb))
 $(eval $(call external_library_source,http://downloads.sourceforge.net/project/sbml/libsbml/$(LIBSBML_VERSION)/stable,libSBML-$(LIBSBML_VERSION)-core-src.zip,76d6c1e9bbe966204db602d9595b6536fc96ff6af7404d2ca1df3225a0a721b4))
 $(eval $(call external_library_source,https://download.gnome.org/sources/libxml2/$(LIBXML2_MAJOR_MINOR_VERSION),libxml2-$(LIBXML2_VERSION).tar.xz,276130602d12fe484ecc03447ee5e759d0465558fbc9d6bd144e3745306ebf0e))
 $(eval $(call external_library_source,https://github.com/google/protobuf/releases/download/v$(PROTOBUF_VERSION),protobuf-cpp-$(PROTOBUF_VERSION).tar.gz,7308590dbb95e77066b99c5674eed855c8257e70658d2af586f4a81ff0eea2b1))
