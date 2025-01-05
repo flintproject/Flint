75979601916ee15ac4ec51622b93aeef807d4918
Takeshi Abe <tabe@fixedpoint.jp>
2025-01-06 00:04:12 +0900

    Include <cstdint> to fix error: 'std::uint32_t' has not been declared

--- source/src/uuidgen.cc
+++ source/src/uuidgen.cc
@@ -1,6 +1,7 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- vim:set ts=4 sw=4 sts=4 noet: */
 #include "uuidgen.h"
 
+#include <cstdint>
 #include <cstdio>
 #include <iostream>
 #include <memory>
